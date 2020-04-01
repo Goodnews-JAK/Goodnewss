445347b3c7730820211646823f7c640b8f240fbf
Goodnews-JAK <62715598+Goodnews-JAK@users.noreply.github.com>
2020-04-01 00:46:00 -1200

    Add files via upload

---
+++ translate.js
@@ -0,0 +1,9 @@
+function convertCase(changeCase){
+    if(changeCase=="upper"){
+        document.formm.enter.value = document.formm.enter.value.toUpperCase()
+    }
+    else{
+        document.formm.enter.value = document.formm.enter.value.toLowerCase()
+    }
+}
+
